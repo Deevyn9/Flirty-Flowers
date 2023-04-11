4f02c6c6909d5efeac4ed94b146dc0dda5cde1c7
Divine Obeten <divineobeten9@gmail.com>
2023-04-11 10:28:38 +0100

    creating the featured section with two image divs

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import "./style.css"
+import photos from "../../data" 
 
 function Featured() {
+  const [firstUrl, secondUrl] = photos;
   return (
-    <div className='featured'>
-      
-    </div>
+    <section className='featured-section'>
+        <div className='featured-row-layout'>
+          <h6>green</h6>
+          <img src={firstUrl} alt="first Image" />
+        </div>
+        <div className='featured-column-layout'>
+          <h6>lily</h6>
+
+          <img src={secondUrl} alt="second image" />
+        </div>
+    </section>
   )
 }
 import "./style.css"
