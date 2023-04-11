a6c677d90376dad4ff4317c341b460b70430b39c
Divine Obeten <divineobeten9@gmail.com>
2023-04-11 09:41:02 +0100

    creating header and making it responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import "./style.css"
 
+import gsap from 'gsap'
+
 function Header() {
   return (
-    <div className='header'>
-      
-    </div>
+    <section className='header-container'>
+      <ul className="header-menu">
+        <li>Intro</li>
+        <li>About</li>
+        <li>Featured</li>
+      </ul>
+      <h1 id='header-text'>Art Objects</h1>
+    </section>
   )
 }
 import "./style.css"
