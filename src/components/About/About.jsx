cb6f339d6c6059f84485dd427c3ae2b58be3c676
Divine Obeten <divineobeten9@gmail.com>
2023-04-11 18:17:32 +0100

    creating custom header style and about styling

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import SectionHeader from '../SectionHeader'
 import "./style.css"
 
 function About() {
   return (
-    <div className='about'>
-      
-    </div>
+    <section className={`about-section`}>
+      <SectionHeader title='about' />
+      <p id="headline">
+        Flirty Flowers is a blog about flowers and the floral designers who make them into art. Creativity and the art of 'making' require dialogue. The full purpose of the Flirty Flowers blog is to encourage and inspire. We value art, we value insight, and we value opinion. 
+      </p>
+    </section>
   )
 }
 import "./style.css"
