842dce2e1e738b165cc78fd2ef4c39a86644bb9a
Divine Obeten <divineobeten9@gmail.com>
2023-04-12 08:36:07 +0100

    fixing gallery display bug

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import "./style.css"
+import React, { useState } from "react";
+import "./style.css";
 
 const images = [
   {
@@ -28,12 +28,46 @@ const images = [
   },
 ];
 
-function Gallery() {
+function GalleryItem({
+  src,
+  category,
+  subtitle,
+  title,
+  updateActiveImage,
+  index,
+}) {
   return (
-    <div className='gallery'>
-      
+    <div className="gallery-item-wrapper">
+      <div className="gallery-item">
+        <div className="gallery-item-info">
+          <h1 className="gallery-info-title">{title}</h1>
+          <h6 className="gallery-info-subtitle">{subtitle}</h6>
+          <p className="gallery-info-category">{category}</p>
+        </div>
+        <div className="gallery-item-image" style={{ backgroundImage: `url(${src})` }}></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Gallery
+function Gallery() {
+  const [activeImage, setActiveImage] = useState(1);
+
+  return (
+    <section className="section-wrapper gallery-wrap">
+      <div className="gallery">
+        <div className="gallery-counter"></div>
+        {images.map((image, index) => (
+          <GalleryItem
+            key={image.src}
+            index={index}
+            {...image}
+            updateActiveImage={(index) => setActiveImage(index + 1)}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Gallery;
