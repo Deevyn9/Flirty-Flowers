df4e6f79eff523447c8ef0e346ce9253a12e7516
Divine Obeten <divineobeten9@gmail.com>
2023-04-12 09:14:00 +0100

    creating and styling footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import "./style.css"
+import SectionHeader from "../SectionHeader"
 
 function Footer() {
   return (
-    <div className='footer'>
+    <section className='footer'>
       
-    </div>
+      <SectionHeader title='Made in'/>
+      <h1 className="location" id='location-text'>
+        Rio di Janeiro
+      </h1>
+    </section>
   )
 }
 import "./style.css"
