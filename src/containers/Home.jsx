c4570286682f670feb34f1455669e4f2921c5a39
Divine Obeten <divineobeten9@gmail.com>
2023-04-13 09:43:00 +0100

    adding a loading screen so pictures load first

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Navbar from '../components/Navbar/Navbar';
 import Header from '../components/Header/Header';
@@ -9,18 +9,27 @@ import Footer from '../components/Footer/Footer';
 import CustomCursor from '../CustomCursor'
 
 function Home() {
+  const [preloader, setPreloader] = useState(true)
   return (
     <>
       <CustomCursor />
 
-      <div className="main-container" id="main-container">
-        <Navbar />
-        <Header />
-        <Featured />
-        <About />
-        <Gallery />
-        <Footer />
-      </div>
+      {preloader ? (
+        <div className="loader-wrapper absolute">
+          <h1>Flirty flowers</h1>
+          <h2>Rio de Janeiro</h2>
+        </div>
+      ) : (
+        <div className="main-container" id="main-container">
+          <Navbar />
+          <Header />
+          <Featured />
+          <About />
+          <Gallery />
+          <Footer />
+        </div>
+      )}
+
     </>
   );
 }
